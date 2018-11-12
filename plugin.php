0cd8fd8716bb38bee54887a870360e81d08ae6a8
K. Adam White <kadamwhite@users.noreply.github.com>
2018-11-12 13:57:46 -0500

    Fix plugin author name formatting

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * WordPress Block Editor Hot Module Replacement Demo
  *
- * @author    K.Adam White <adam@kadamwhite.com>
+ * @author    K. Adam White
  * @license   GPL-2.0+
  * @copyright 2018 K.Adam White
  *
@@ -10,8 +10,8 @@
  * Plugin Name: WordPress Block Editor Hot Module Replacement Demo
  * Description: A plugin demonstrating block and plugin auto-loading with hot module replacement.
  * Version:     0.1.0
- * Author:      K.Adam White <adam@kadamwhite.com>
- * Author URI:  http://kadamwhite.com
+ * Author:      K. Adam White
+ * Author URI:  https://www.kadamwhite.com
  * License:     GPL-2.0+ or Artistic License 2.0
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
